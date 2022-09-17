aa6d962643579aea45b32f6dbe175995bf74bb9c
Harsh <harshagrawal23601@gmail.com>
2022-09-17 22:22:50 +0530

    My Book List App
    A simple Book list App created to store and record the list of books with some minor details.
    Do check it out and have fun!!

---
+++ app.js
@@ -0,0 +1,139 @@
+//Book Class:Represents a book
+class Book{
+    constructor(title, author, isbn){
+        this.title=title;
+        this.author=author;
+        this.isbn=isbn;
+    }
+}
+
+
+//UI class Handles Ui tasks
+class UI{
+static displayBooks(){
+    
+
+       const books=Store.getBooks();
+
+       books.forEach((book)=>UI.addBookToList(book));
+
+      }
+     static  addBookToList(book){
+        const list=document.querySelector('#book-list');
+        
+        const row=document.createElement('tr');
+        row.innerHTML=`
+        <td>${book.title}</td>
+        <td>${book.author}</td>
+        <td>${book.isbn}</td>
+        <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
+
+        `;
+        list .appendChild(row);
+
+     }
+     static deleteBook(el){
+         if(el.classList.contains('delete')){
+             el.parentElement.parentElement.remove();
+         }
+     }
+
+     static showAlert(message, className){
+         const div=document.createElement('div');
+         div.className=`alert alert-${className}`;
+         div.appendChild(document.createTextNode(message));
+        const conntainer=document.querySelector('.container')
+        const form=document.querySelector('#book-form');
+        conntainer.insertBefore(div,form);
+       //Vanish in 3 seconds
+       setTimeout(()=> document.querySelector('.alert').remove(),
+            3000);  
+      
+    }
+
+     static clearFileds(){
+         document.querySelector('#title').value='';
+         document.querySelector('#author').value='';
+         document.querySelector('#isbn').value='';
+     }
+}
+//Store Class: Handles Storage
+class Store{
+    static getBooks(){
+          let books;
+          if(localStorage.getItem('books')===null){
+              books=[];
+          }else{
+              books=JSON.parse(localStorage.getItem('books'));
+          }
+          return books;
+    }
+   static addBooks(book) {
+         const books=Store.getBooks();
+         books.push(book);
+
+         localStorage.setItem('books',JSON.stringify(books))
+    }
+   static  removeBook(isbn){
+       const books=Store.getBooks();
+       books.forEach((book,index)=>{
+           if(book.isbn===isbn){
+                books.splice(index,1);  
+           }
+       });
+
+       localStorage.setItem('books',JSON.stringify(books));
+    }
+}
+
+
+
+
+
+//Event: Display Books
+document.addEventListener('DOMContentLoaded', UI.displayBooks);
+
+
+
+//Events: Add a Book
+document.querySelector('#book-form').addEventListener('submit',(e)=> { 
+    //Pevent Actual Submit
+    e.preventDefault();
+    
+    //Get Form Values
+    const title = document.querySelector('#title').value;
+    const author = document.querySelector('#author').value;
+    const isbn = document.querySelector('#isbn').value;
+    
+
+    //Validate
+   if(title === '' || author==='' || isbn===''){
+       UI.showAlert('Please fill all the fields!!','danger');
+}else{
+           //Instantiate Book
+    const book=new Book(title,author, isbn);
+
+    //Add Book to UI
+    UI.addBookToList(book);
+    //Add book to Store
+    Store.addBooks(book);
+//Show Success Message
+UI.showAlert('Book Added Successfully!!','success');
+
+    //Clear Fields
+    UI.clearFileds();   
+}
+
+});
+
+//Event: Remove a Book
+document.querySelector("#book-list").addEventListener('click',(e)=>{
+   UI.deleteBook(e.target);
+  //Delete Book From Store
+
+     Store.removeBook(e.target.parentElement.previousElementSibling.textContent)
+
+
+   //Show Success Message
+UI.showAlert('Book Removed Successfully!!','success');
+})
